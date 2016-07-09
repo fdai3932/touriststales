b695e7898fec158ffe1374848cc1f0b945df8e4d
Achille <Achille@BBDamour>
2016-07-09 08:46:45 +0200

    update TestDB demo for Kolloqium commit2

--- src/test/java/com/mycompany/touriststales/classPackage/DBTest.java
+++ src/test/java/com/mycompany/touriststales/classPackage/DBTest.java
@@ -37,7 +37,9 @@ public class DBTest {
         System.out.println("DBTest: GetInstance");
         DB expResult = new DB();
         DB result = DB.getInstance();
-        
+        System.err.println(expResult.err);
+        System.err.println("----------------------");
+        System.err.println(result.err);
         System.err.println("If the instantiation of the object work fine, expresult and result should be the same object");
         
         assertSame(expResult, result); //both expResult and Result should refer to the same object
